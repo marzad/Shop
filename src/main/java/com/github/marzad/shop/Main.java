cecac83a478bac285a32750d30450cf20f00126b
vladimirklippert <vladimir.klippert93@gmail.com>
2022-11-04 13:01:58 -0700

    Created workspace.xml, Updated pom.xml

--- src/main/java/com/github/marzad/shop/Main.java
+++ src/main/java/com/github/marzad/shop/Main.java
@@ -1,9 +1,11 @@
-package com.github.marzad.shop;public class Main {
-    public static void main(String[] args) {
-        ProductRepo prodRep=new ProductRepo();
+package com.github.marzad.shop;
 
-        Product prod1 = new Product(1,"Apfel");
-        Product prod2 = new Product(2,"Banane");
+public class Main {
+    public static void main(String[] args) {
+        ProductRepo prodRep = new ProductRepo();
+
+        Product prod1 = new Product(1, "Apfel");
+        Product prod2 = new Product(2, "Banane");
         prodRep.addEle(prod1);
         prodRep.addEle(prod2);
 
--- src/main/java/com/github/marzad/shop/ProductRepo.java
+++ src/main/java/com/github/marzad/shop/ProductRepo.java
@@ -3,23 +3,24 @@ package com.github.marzad.shop;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductRepo extends Product{
+public class ProductRepo extends Product {
     private List<Product> products = new ArrayList();
 
-    public void addEle(Product name){
+    public void addEle(Product name) {
         products.add(name);
     }
 
-    public List<Product> list(){
+    public List<Product> list() {
         return products;
     }
-    public Product get(int i){
+
+    public Product get(int i) {
         return products.get(i);
     }
-    public Product getProdByName(String name){
-        for (Product s: this.products)
-        {
-            if(s.getName().equals(name)) {
+
+    public Product getProdByName(String name) {
+        for (Product s : this.products) {
+            if (s.getName().equals(name)) {
                 return s;
             }
         }
