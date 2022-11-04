c26f58479510bb645335b5585e565189a37b3d99
vladimirklippert <vladimir.klippert93@gmail.com>
2022-11-04 15:24:52 -0700

    Added ProductInterface for bonus

---
+++ src/main/java/com/github/marzad/shop/ProductInterface.java
@@ -0,0 +1,12 @@
+package com.github.marzad.shop;
+
+public interface ProductInterface {
+    void setId();
+    int getId();
+    String getName();
+    void setName();
+    double getPrice();
+    void setPrice();
+
+
+}
