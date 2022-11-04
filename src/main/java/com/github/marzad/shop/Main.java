ae886c8804f62e0604b163967c73ece1aefe41d2
vladimirklippert <vladimir.klippert93@gmail.com>
2022-11-04 13:13:15 -0700

    Updated Main

--- src/main/java/com/github/marzad/shop/Main.java
+++ src/main/java/com/github/marzad/shop/Main.java
@@ -3,11 +3,25 @@ package com.github.marzad.shop;
 public class Main {
     public static void main(String[] args) {
         ProductRepo prodRep = new ProductRepo();
-
+        OrderRepo ordRep = new OrderRepo();
         Product prod1 = new Product(1, "Apfel");
         Product prod2 = new Product(2, "Banane");
+        Product prod3 = new Product(3, "Birne");
         prodRep.addEle(prod1);
         prodRep.addEle(prod2);
+        prodRep.addEle(prod3);
+        /*System.out.println(prodRep.list());
+
+        System.out.println(prodRep.getProdByName("Apfel"));
+        System.out.println("GetMethode by index 2: "+ prodRep.get(2));*/
+
+        Order ord1 = new Order(1, prod1);
+        Order ord2 = new Order(2, prod2);
+        Order ord3 = new Order(3, prod3);
+        ordRep.addEle(ord1);
+        ordRep.addEle(ord2);
+        ordRep.addEle(ord3);
+        System.out.println("Liste der Bestellungen: "+ordRep.list());
 
     }
 }
--- src/main/java/com/github/marzad/shop/OrderRepo.java
+++ src/main/java/com/github/marzad/shop/OrderRepo.java
@@ -6,19 +6,21 @@ import java.util.List;
 public class OrderRepo {
     private List<Order> orders = new ArrayList();
 
-    public void addEle(Order name){
+    public void addEle(Order name) {
         orders.add(name);
     }
-    public List<Order> list(){
+
+    public List<Order> list() {
         return orders;
     }
-    public Order get(int i){
+
+    public Order get(int i) {
         return orders.get(i);
     }
-    public Order getOrderByName(String name){
-        for (Order s: this.orders)
-        {
-            if(s.getName().equals(name)) {
+
+    public Order getOrderByName(String name) {
+        for (Order s : this.orders) {
+            if (s.getName().equals(name)) {
                 return s;
             }
         }
